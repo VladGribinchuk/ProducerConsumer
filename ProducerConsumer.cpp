54121ecdefe3f694dbf1af0ed81a9d6e74f3495a
IlliaHryshchuk <45259941+IlliaHryshchuk@users.noreply.github.com>
2020-04-26 22:13:06 +0300

    added spreadsheets + parallelism
    added spreadsheets + parallelism

--- ProducerConsumer.cpp
+++ ProducerConsumer.cpp
@@ -1,16 +1,34 @@
-#include <iostream> //test commiting
+#include <iostream>
 #include <functional>
 #include <queue>
 #include <mutex>
-// how does it work??
-//smth
-//smth
-//smth
-//smth
-
 #include <condition_variable>
-//end commiting
 #include <sstream>
+#include "Spreadsheet/Spreadsheet.h"
+#include <time.h>
+
+
+class MyTimer
+{
+public:
+	MyTimer() {}
+	~MyTimer() {}
+	void startTimer()
+	{
+		start = std::chrono::high_resolution_clock::now();
+	}
+	double stopTimer()
+	{
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+		double result = duration.count();
+		return result;
+	}
+private:
+	std::chrono::time_point<std::chrono::steady_clock> start, end;
+	std::chrono::duration<float> duration;
+};
+
 
 // Logging without corruption.
 // Supposed to be used in a multi-threading environment.
@@ -18,8 +36,7 @@
 class ThreadSafeLogger
 {
 public:
-	ThreadSafeLogger(std::ostream& outputStream = std::cout)
-		: os(outputStream)
+	ThreadSafeLogger(std::ostream& outputStream = std::cout) : os(outputStream)
 	{
 		ss << "[TRACE] ";
 	}
@@ -56,25 +73,28 @@ public:
 	using ProducerFunc = std::function<T()>;
 	using ConsumerFunc = std::function<void(const T&)>;
 
-	ProducerConsumer(ProducerFunc produce, ConsumerFunc consume)
-		: produce(produce),
-		consume(consume)
-	{}
+	ProducerConsumer(ProducerFunc produce, ConsumerFunc consume) : produce(produce), consume(consume) {}
 
 	void run(int itemCount)
 	{
-		while (itemCount-->0)
+#pragma omp parallel
 		{
-			T item = produce();
-			buffer.push(item);
-
-			item = buffer.pop();
-			consume(item);
+#pragma omp for
+			for (int i = 0; i < itemCount; i++)
+			{
+				T item = produce();
+#pragma omp critical
+				{
+					buffer.push(item);
+					item = buffer.pop();
+				}
+				consume(item);
+			}
 		}
 	}
 
 private:
-	template <typename Elem> 
+	template <typename Elem>
 	class QueueSynchronized
 	{
 	public:
@@ -90,7 +110,7 @@ private:
 			std::unique_lock<std::mutex> lock(mut);
 			while (q.empty())
 				cond.wait(lock);
-			
+
 			Elem val = q.front();
 			q.pop();
 			return val;
@@ -115,25 +135,53 @@ private:
 };
 
 
-struct Dummy { int num; static int glob; };
-int Dummy::glob = 0;
+struct SpreadsheetContainer { Spreadsheet spreadsheet; int num; static int globalCounter; };
+int SpreadsheetContainer::globalCounter = 0;
+
 
 int main()
 {
-	auto produce = []() { 
-		Dummy item{ Dummy::glob++ };
-		TS_LOG() << "generating Dummy " << item.num;
+	srand((unsigned)time(NULL));
+
+	auto produce = []() {
+		SpreadsheetContainer item;
+		item.num = SpreadsheetContainer::globalCounter++;
+		item.spreadsheet.resize(rand() % 6 + 3, rand() % 6 + 3);
+
+		item.spreadsheet = SpreadsheetGenerator::generate(item.spreadsheet.getRows(), item.spreadsheet.getColumns());
+#pragma omp critical 
+		{
+			TS_LOG() << "generating spreadsheet #" << item.num;
+			item.spreadsheet.print();
+		}
 		return item;
 	};
 
-	auto consume = [](const Dummy& item) {
-		TS_LOG() << "calculating dummy " << item.num;
+	auto consume = [](const SpreadsheetContainer& item) {
+		SpreadsheetContainer calculatedItem;
+		calculatedItem.spreadsheet.resize(item.spreadsheet.getRows(), item.spreadsheet.getColumns());
+
+		calculatedItem.spreadsheet = SpreadsheetCalculator::calculateSpreadsheet(item.spreadsheet);
+#pragma omp critical
+		{
+			TS_LOG() << "calculating spreadsheet #" << item.num;
+			calculatedItem.spreadsheet.print();
+		}
 	};
 
-	constexpr int MAX_ITEM_TO_PRODUCE = 5;
-	ProducerConsumer<Dummy> problem(produce, consume);
+
+	MyTimer timer; timer.startTimer();
+	constexpr int MAX_ITEM_TO_PRODUCE = 100;
+	ProducerConsumer<SpreadsheetContainer> problem(produce, consume);
 	problem.run(MAX_ITEM_TO_PRODUCE);
+	std::cout << "\n\n\nTIME: " << timer.stopTimer() << std::endl;
 
 	system("pause");
 	return 0;
 }
+
+//ТЗ
+//замість заглушок (Dummy) необхідно використати Spreadsheet, тобто налаштувати клас ProducerConsumer для роботи зі Spreadsheet об'єктами. 
+//використовуючи засоби OpenMP, перепроектувати на багатопоточність(ProducerConsumer::run() метод), так щоб багато потоків паралельно постачали(produce) визначену кількість 
+//таблиць і багато потоків паралельно споживали(consume) їх. В якості функції produce у вас має бути генерація рандомної таблиці, а функції consume - обчислення таблиці.
+//Не забудьте невеличку синхронізацію між потоками - має бути спродюсовано тільки визначену кількість itemCount таблиць; не має виникати жодних deadlock-ів; виконання не повинне зациклюватись.
